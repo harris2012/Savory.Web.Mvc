0d70eb11bf5ac8fb793837f4091eeba09958809b
HarrisZhang <hc_zhang@Ctrip.com>
2018-03-05 14:18:20 +0800

    update

---
+++ Savory.Web.Mvc/SavoryJsonResult.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Savory.Web.Mvc
+{
+    /// <summary>
+    /// An action result which formats the given object as JSON.
+    /// </summary>
+    public class SavoryJsonResult : ActionResult
+    {
+        public SavoryJsonResult()
+        {
+            this.JsonRequestBehavior = JsonRequestBehavior.DenyGet;
+        }
+
+        public override void ExecuteResult(ControllerContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException("context");
+            }
+            if ((this.JsonRequestBehavior == JsonRequestBehavior.DenyGet) && string.Equals(context.HttpContext.Request.HttpMethod, "GET", StringComparison.OrdinalIgnoreCase))
+            {
+                throw new InvalidOperationException("GET is NOT allowed");
+            }
+            HttpResponseBase response = context.HttpContext.Response;
+            if (!string.IsNullOrEmpty(this.ContentType))
+            {
+                response.ContentType = this.ContentType;
+            }
+            else
+            {
+                response.ContentType = "application/json";
+            }
+            if (this.ContentEncoding != null)
+            {
+                response.ContentEncoding = this.ContentEncoding;
+            }
+            if (this.Data != null)
+            {
+                var jsonString = JsonConvert.SerializeObject(this.Data);
+
+                response.Write(jsonString);
+            }
+        }
+
+        public Encoding ContentEncoding { get; set; }
+
+        public string ContentType { get; set; }
+
+        public object Data { get; set; }
+
+        public JsonRequestBehavior JsonRequestBehavior { get; set; }
+    }
+}
